cc2f626d231ef5af76985e9c17ae76236cd56511
coder-abdo <a.h2020.2007@gmail.com>
2023-02-20 19:37:34 +0200

    solve the longest prefix problem

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,16 @@
 //import { runningSum } from "./sumOfRunningArray";
 //import {finalValue} from './finalValue'
 //import {twoSum} from './twoSum'
+// import { lengthOfLastWord } from "./lengthOfLastWord";
 //import { isAnagram } from "./anagram";
-import { isSupsqeunce } from "./isSubsquence";
+//import { isSupsqeunce } from "./isSubsquence";
 //import { replaceElements } from "./replaceElementWithTheGreates";
 //console.log(isAnagram("car", "rat"))
 //console.log(isAnagram("anagram", "nagaram"))
 //console.log(replaceElements([17,18,5,4,6,1]))
-console.log(isSupsqeunce("abc", "ahbgdc"));
-console.log(isSupsqeunce("ab", "baab"));
-console.log(isSupsqeunce("abc", "acb"));
+// console.log(isSupsqeunce("abc", "ahbgdc"));
+// console.log(isSupsqeunce("ab", "baab"));
+// console.log(isSupsqeunce("abc", "acb"));
 //console.log(twoSum([3, 2, 3], 6))
 //console.log(runningSum([1, 1, 1, 1]))
 //console.log(generateRowsOfPascal(5))
@@ -22,4 +23,12 @@ console.log(isSupsqeunce("abc", "acb"));
 //console.log(missingNumber([0]))
 //console.log(thirdMaxNum([3, 2, 1]))
 //console.log(arrayPairSum([1,2,3,2]))
+
+import { longestCommonPrefix } from "./longestCommonPrefix";
+
 //console.log(arrayPairSum([6,2,6,5,1,2]))
+// console.log(lengthOfLastWord("hello world"));
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+console.log(longestCommonPrefix(["a"]));
+console.log(longestCommonPrefix(["", ""]));
---
+++ src/lengthOfLastWord.ts
@@ -0,0 +1,4 @@
+export const lengthOfLastWord = (s: string): number => {
+  const words = s.trim().split(" ");
+  return words[words.length - 1].length;
+};
